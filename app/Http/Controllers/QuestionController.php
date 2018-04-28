ec6d44ea14eacf1044149fada028dc161e7e1093
Muhammad Lathif Pambudi <lathif.pambudi@tokopedia.com>
2018-04-28 16:51:55 +0700

    Add app folder

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Question;
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    public function index(Request $request)
+    {
+        $sort = $request->input('sort');
+        $order = $request->input('order');
+        if (!$sort) {
+            $sort = 'label';
+            $order = 'asc';
+        }
+        $questions = Question::orderBy($sort, $order)->paginate(20);
+        $links = $questions->appends(['sort' => $sort, 'order' => $order])->links();
+        return view('admin.question.index', compact('questions', 'sort', 'order', 'links'));
+    }
+}
---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+use Lang;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return view('admin.tasks.index', compact('tasks', $tasks));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.tasks.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required|min:3',
+            'description' => 'max:200',
+        ]);
+
+        $task = Task::create([
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
+        return redirect()->route('admin.tasks.show', $task);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        return view('admin.tasks.show', compact('task', $task));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Task $task)
+    {
+        return view('admin.tasks.edit', compact('task', $task));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Task $task)
+    {
+        $this->validate($request, [
+            'title' => 'required|min:3',
+        ]);
+        $task->title = $request->title;
+        $task->description = $request->description;
+        $task->save();
+        $request->session()->flash('message', Lang::get('global.tasks.saved'));
+        return redirect()->route('admin.tasks.show', $task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, Task $task)
+    {
+        $task->delete();
+        $request->session()->flash('message', Lang::get('global.tasks.deleted'));
+        return redirect()->route('admin.tasks.index');
+    }
+
+    /**
+     * Delete all selected Task at once.
+     *
+     * @param Request $request
+     */
+    public function massDestroy(Request $request)
+    {
+        if (!Gate::allows('users_manage')) {
+            return abort(401);
+        }
+        if ($request->input('ids')) {
+            $entries = Task::whereIn('id', $request->input('ids'))->get();
+
+            foreach ($entries as $entry) {
+                $entry->delete();
+            }
+        }
+    }
+}
