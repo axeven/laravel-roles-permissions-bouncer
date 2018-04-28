47efd1afe5a9eb029eccc50858b0c1d9e4df4c31
Muhammad Lathif Pambudi <lathif.pambudi@tokopedia.com>
2018-04-28 16:53:03 +0700

    Add routes folder

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-Route::get('/', function () { return redirect('/admin/home'); });
+
+Route::get('/', 'HomeController@index');
 
 // Authentication Routes...
 $this->get('login', 'Auth\LoginController@showLoginForm')->name('auth.login');
@@ -17,12 +18,13 @@ $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 $this->post('password/reset', 'Auth\ResetPasswordController@reset')->name('auth.password.reset');
 
 Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::get('/home', 'HomeController@index');
     Route::resource('abilities', 'Admin\AbilitiesController');
     Route::post('abilities_mass_destroy', ['uses' => 'Admin\AbilitiesController@massDestroy', 'as' => 'abilities.mass_destroy']);
     Route::resource('roles', 'Admin\RolesController');
     Route::post('roles_mass_destroy', ['uses' => 'Admin\RolesController@massDestroy', 'as' => 'roles.mass_destroy']);
     Route::resource('users', 'Admin\UsersController');
     Route::post('users_mass_destroy', ['uses' => 'Admin\UsersController@massDestroy', 'as' => 'users.mass_destroy']);
-
+    Route::resource('tasks', 'TaskController');
+    Route::post('tasks_mass_destroy', ['uses' => 'TasksController@massDestroy', 'as' => 'tasks.mass_destroy']);
+    Route::resource('questions', 'QuestionController');
 });
